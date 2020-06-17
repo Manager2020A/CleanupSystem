8905cdb2aaa8c62799c428c76c2289f6f3cbd8ac
chriskroth <christiankroth@gmail.com>
2020-06-16 22:10:31 -0300

    Teste

--- src/main/java/com/man/cleanup/service/ProductService.java
+++ src/main/java/com/man/cleanup/service/ProductService.java
@@ -36,6 +36,12 @@ public class ProductService {
     }
 
     @GET
+    @Path("/teste")
+    public List<Product> findAllTeste() {
+        return Product.listAll();
+    }
+
+    @GET
     @Path("/active")
     public List<Product> findAllActives(){
         return Product.list("active", true);
